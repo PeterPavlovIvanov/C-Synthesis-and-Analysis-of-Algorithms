a3db33a55567e89c9c0f787248365a7e5e60334b
Peter <47712814+PeterPavlovIvanov@users.noreply.github.com>
2022-09-13 17:55:03 +0300

    Pocedures
    ADDED
    login auth
    register username availability check
    TODO:
    actual login
    register insert a customer

---
+++ BETTERCALLTAXI/BetterCallTaxi/BetterCallTaxi/GlobalConstants.cs
@@ -0,0 +1,34 @@
+﻿
+namespace BetterCallTaxi
+{
+
+    class GlobalConstants
+    {
+        public const string CONNECTION_STRING = "Data Source=DESKTOP-PFQL6JD;Database=BetterCallTaxi;Integrated Security=True";
+
+        // Queries
+        public const string SELECT_CUSTOMER_BY_USERNAME = "SELECT * FROM CUSTOMERS WITH(NOLOCK) WHERE USERNAME = '{0}'";
+
+        // Procedures
+        public const string LOGIN_AUTHENTICATION = "LOGIN_AUTHENTICATION";
+        public const string CHECK_USERNAME_AVAILABILITY = "CHECK_USERNAME_AVAILABILITY";
+
+        // User Feedback Messages
+        public const string INVALID_CREDENTIALS = "Invalid credentials!";
+        public const string USERNAME_TAKEN = "The username: '{0}' is not available!";
+        public const string UCN_REQUIRED = "UCN is required.";
+        public const string UCN_LENGTH_MESSAGE = "UCN is a 10 digit code.";
+        public const string UCN_DIGITS_MESSAGE = "UCN is a 10 digit code.";
+
+        // Codes
+        public const int UCN_LENGTH = 10;
+        public const int SUCCESSFUL = 1;
+        public const int ERROR = 0;
+        public const int ERROR_ = -1;
+
+        public GlobalConstants()
+        {
+
+        }
+    }
+}
---
+++ BETTERCALLTAXI/BetterCallTaxi/BetterCallTaxi/InputValidator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetterCallTaxi
+{
+    class InputValidator
+    {
+        public InputValidator()
+        {
+
+        }
+
+        public static bool Validate_Username(string strUsername)
+        {
+            if (strUsername == "")
+            {
+                MessageBox.Show("You must enter a username.");
+                return false;
+            }
+
+            if (strUsername.Length < 5)
+            {
+                MessageBox.Show("You must enter a username longer than 4 symbols.");
+                return false;
+            }
+
+            if (strUsername.Length > 25)
+            {
+                MessageBox.Show("You must enter a username shorter than 26 symbols.");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool Validate_Password(string strPassword)
+        {
+            if (strPassword == "")
+            {
+                MessageBox.Show("You must enter a password.");
+                return false;
+            }
+
+            if (strPassword.Length < 6)
+            {
+                MessageBox.Show("You must enter a password longer than 5 symbols.");
+                return false;
+            }
+
+            if (strPassword.Length > 25)
+            {
+                MessageBox.Show("You must enter a password shorter than 26 symbols.");
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool Validate_Ucn(string strUcn)
+        {
+            if (strUcn == "")
+            {
+                MessageBox.Show(GlobalConstants.UCN_REQUIRED);
+                return false;
+            }
+
+            if (strUcn.Length != GlobalConstants.UCN_LENGTH)
+            {
+                MessageBox.Show(GlobalConstants.UCN_LENGTH_MESSAGE);
+                return false;
+            }
+
+            try
+            {
+                Int32.Parse(strUcn);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show(GlobalConstants.UCN_DIGITS_MESSAGE);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
